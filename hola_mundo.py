7aba99fbf51cbbc3067cea8f0a3051f23bd41ba3
M2003rp <manue03l@hotmail.com>
2020-04-22 18:43:08 -0500

    Sin error

--- hola_mundo.py
+++ hola_mundo.py
@@ -1,7 +1,4 @@
 print( "hola mundo " )
 print('Esta es mi modificación | David')
-<<<<<<< HEAD
 print('Esta es mi modificacion , Manuel')
-=======
 print("¡Hola, mundo! Ésta es mónica)
->>>>>>> 60ef52ecf7300f9f8eb3ab401a5493fe49612b09
